95f2d85bda43d0fcd765d07fefb3cdf28b2fbfe1
Marcos Martins Carvalho <36687@ma-web.nl>
2023-11-21 15:19:01 +0100

    opdracht3

---
+++ 01B/app.js
@@ -0,0 +1,32 @@
+class App {
+    runApplication() {
+        this.greeting = "starting up";
+
+    }
+
+}
+
+
+
+
+let app = new App();
+app.runApplication();
+
+let appnaam = ("nerve gear");
+let versienummer = ("1");
+let versiedatum = ("mei 2022");
+let autheur = ("Kayaba Akihiko");
+let copyright = ("NerveGear");
+let distributeur = ("Kayaba Akihiko");
+let darkmode = ("false ");
+
+
+console.log(app.greeting)
+console.log("appnaam: " + appnaam);
+console.log("versienummer: " + versienummer);
+console.log("versiedatum: " + versiedatum);
+console.log("copyright: " + copyright);
+console.log("autheur: " + autheur);
+console.log("distributeur: " + distributeur);
+console.log("darkmode: " + darkmode);
+
