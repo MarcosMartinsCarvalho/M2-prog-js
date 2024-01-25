3e31811902e9a959602b040b38b94041c3892b35
Marcos Martins Carvalho <36687@ma-web.nl>
2024-01-25 14:36:40 +0100

    les4b

--- les4/app.js
+++ les4/app.js
@@ -2,15 +2,13 @@ class App {
     runApplication() {
         console.clear();
 
-
-        function bam() {
-            console.log("bam");
+        function argumentsAreHandy(shoutout,shoutout2,shoutout3){
+            console.log("could you give me a shoutout?");
+            console.log(shoutout);
+            console.log(shoutout2);
+            console.log(shoutout3);
         }
-        bam();
-        bam();
-        bam();
-        bam();
-        bam();
+        argumentsAreHandy("mega super shoutout","biembiembambam", "super mega turbo giga astro shoutout");
     }
 }
 
