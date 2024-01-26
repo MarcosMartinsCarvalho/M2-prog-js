8c808f5a4cb253725f1468651c94708d08aba1ac
Marcos Martins Carvalho <36687@ma-web.nl>
2024-01-26 13:55:04 +0100

    les6af

--- les6/events/app.js
+++ les6/events/app.js
@@ -1,12 +1,23 @@
-class App
-{
-    runApplication()
-    {
-        console.log("hello world!");
-        let uiButton = document.getElementById("myButton")
-        uiButton.addEventListener("click",(e)=>{
-            console.log("click!")
-        })
+class App {
+    runApplication() {
+        let button = document.getElementById('mijnKnop');
+        let content = document.getElementById('alles');
+        let biemdiv = document.getElementById('biemdiv');
+        let biemdiv1 = document.getElementById('biemdiv1');
+
+        button.addEventListener('click', () => { 
+            content.hidden = false;
+            
+        });
+
+        biemdiv.addEventListener('click', () => {
+            biemdiv.style.backgroundColor = 'purple';
+            biemdiv1.style.display = 'block';
+            biemdiv2.style.display = "block";
+
+            console.alert
+        });
+        
     }
 }
 
