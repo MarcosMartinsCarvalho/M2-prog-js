597f1f8eb40959824ab10baffb775fc7a6e951d1
Marcos Martins Carvalho <36687@ma-web.nl>
2024-01-26 12:02:44 +0100

    les5d

--- les5/app.js
+++ les5/app.js
@@ -1,22 +1,63 @@
-class App {
-    runApplication() {
+class App
+{
+    runApplication()
+    {
         console.clear();
-        let nummers = [1,2,3]
-        let myarr = ["boem", "biem", "bam"]
+        let headers = document.getElementsByTagName("h1")
+        this.LogArray(headers)
+
+        let headersByCssClass = document.getElementsByClassName("bandName")
+        this.LogArray(headersByCssClass)
+        for (let i = 0; i < headersByCssClass.length; i++) {
+            console.log(headersByCssClass[i].innerHTML)
+            headersByCssClass[i].innerHTML = "bam"
+        }
+
+        headersByCssClass[0].innerHTML = "Nieuws"
+        headersByCssClass[1].innerHTML = "Reviews"
+        headersByCssClass[2].innerHTML = "Commentaar"
+        headersByCssClass[3].innerHTML = "Beste forum posts"
+        headersByCssClass[4].innerHTML = "Pricewatch"
+
+        let data = [headersByCssClass[0].innerHTML, headersByCssClass[1].innerHTML, headersByCssClass[2].innerHTML, headersByCssClass[3].innerHTML, headersByCssClass[4].innerHTML]
+        for (let i = 0; i < data.length; i++) {
+            headersByCssClass[i].innerHTML = `${i}: ${data[i]}`
+        }
+
+        console.log("biem");
+        let band = ["Sycamour", "Deviloof", "Jiluka", "pierce the veil"]
+        console.log(band)
+        this.logMetNummer(band)
+
+        band.push("Nik Nocturnal")
+        band.push("Ice Nine Kills")
+        this.logMetNummer(band)
+        let indexToRemove = band.indexOf("bob dylan")
+        band.splice(indexToRemove, 1)
+
+
+        indexToRemove = band.indexOf("prince")
+        band.splice(indexToRemove, 1)
+
+        band.push("bob dylan")
+        band.push("prince")
+
         
-        let indexToRemove = myarr.indexOf("biem");
-        myarr.splice(indexToRemove, 1);
-        
-        indexToRemove = myarr.indexOf("bam");
-        myarr.splice(indexToRemove, 1);
-        
-        myarr.push("panic at the disco", "sycamour");
-        
-        for(let i = 0; i < myarr.length; i++) {
-            const woordenprint = i + ": " + myarr[i];
-            
-            console.log(woordenprint);
-            
+        this.logMetNummer(band)
+
+        let nummers = [1, 2, 3]
+        this.logMetNummer(nummers)
+    }
+
+    LogArray(array){
+        for (let i = 0; i < array.length; i++) {
+            console.log(array[i])
+        }
+    }
+
+    logMetNummer(arraytje){
+        for (let i = 0; i < arraytje.length; i++) {
+            console.log(`${i + 1}: ${arraytje[i]}`)            
         }
     }
 }
