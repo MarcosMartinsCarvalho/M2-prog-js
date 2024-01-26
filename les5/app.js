e35e0b4e36cfbf0ff4b93272034d044fa072b3e0
Marcos Martins Carvalho <36687@ma-web.nl>
2024-01-26 11:29:47 +0100

    spliceEnPush

--- les5/app.js
+++ les5/app.js
@@ -3,16 +3,23 @@ class App {
         console.clear();
         let nummers = [1,2,3]
         let myarr = ["boem", "biem", "bam"]
+        
+        let indexToRemove = myarr.indexOf("biem");
+        myarr.splice(indexToRemove, 1);
+        
+        indexToRemove = myarr.indexOf("bam");
+        myarr.splice(indexToRemove, 1);
+        
+        myarr.push("panic at the disco", "sycamour");
+        
         for(let i = 0; i < myarr.length; i++) {
             const woordenprint = i + ": " + myarr[i];
-            const nummerprint = nummers[i];
+            
             console.log(woordenprint);
-            console.log(nummerprint);
+            
         }
-
     }
 }
-
 
 let app = new App();
 app.runApplication();
