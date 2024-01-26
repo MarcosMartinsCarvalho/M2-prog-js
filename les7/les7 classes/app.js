8e930fa22af8412598cb91cb82961ffacc734058
Marcos Martins Carvalho <36687@ma-web.nl>
2024-01-26 14:10:20 +0100

    greeting

---
+++ les7/les7 classes/app.js
@@ -0,0 +1,30 @@
+class App {
+    runApplication() {
+        
+
+    }
+}
+
+class Greet
+{
+    constructor()
+    {
+        
+        console.log("greetings");
+    }
+
+}
+class goodbye
+{
+    constructor()
+    {
+        
+        console.log("byebye");
+    }
+
+}
+
+let greet = new Greet()
+let bye = new goodbye()
+let app = new App();
+app.runApplication();
