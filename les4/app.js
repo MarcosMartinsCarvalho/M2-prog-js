42937876a325026cb0e5023f011d436824df24dd
Marcos Martins Carvalho <36687@ma-web.nl>
2024-01-26 10:56:26 +0100

    boom

--- les4/app.js
+++ les4/app.js
@@ -1,36 +1,103 @@
 class App {
-    runApplication() {
-        console.clear();
-        this.mario();
-        bwahaha();
-        optellen(5,6);
-        extra();
-
+    runApp() {
+      const canvas = document.getElementById("canvasId");
+      const g = canvas.getContext("2d");
+      this.drawHouse(g, 10, 10);
+      this.drawHouse(g, 75, 50);
+      this.drawHouse(g, 120, 120);
+      this.drawChristmasTree(g, 300, 300);
     }
-
-    mario()
-    {
-        console.log("MARIOOOOOO");
+  
+    drawHouse(g, x, y) {
+      g.beginPath();
+      g.moveTo(30 + x, 10 + y);
+  
+      
+      this.setColor(g, "red");
+      g.lineTo(70 + x, 20 + y);
+      g.lineTo(60 + x, 40 + y);
+      g.lineTo(20 + x, 30 + y);
+      g.closePath();
+      g.fill();
+      g.stroke();
+  
+      
+      this.setColor(g, "lightGray");
+      g.fill();
+      g.stroke();
+  
+      this.setColor(g, "gray");
+      g.beginPath();
+      g.moveTo(60 + x, 40 + y);
+      g.lineTo(70 + x, 20 + y);
+      g.lineTo(80 + x, 30 + y);
+      g.closePath();
+      g.fill();
+      g.stroke();
+  
+      this.setColor(g, "darkGray");
+      g.beginPath();
+      g.moveTo(60 + x, 40 + y);
+      g.lineTo(60 + x, 60 + y);
+      g.lineTo(80 + x, 50 + y);
+      g.lineTo(80 + x, 30 + y);
+      g.fill();
+  
+      g.stroke();
+  
+      this.setColor(g, "cyan", 0.5);
+      g.fillRect(30 + x, 40 + y, 20, 10);
     }
-
-    bwahaha()
-    {
-        console.log("bwahahaha");
+  
+    drawChristmasTree(g, x, y) {
+      
+      this.setColor(g, "saddleBrown");
+      g.beginPath();
+      g.moveTo(x, y);
+      g.lineTo(x + 50, y);
+      g.lineTo(x + 50, y + 50);
+      g.lineTo(x, y + 50);
+      g.closePath();
+      g.fill();
+      g.stroke();
+  
+      
+      this.setColor(g, "forestGreen");
+      g.beginPath();
+      g.moveTo(x - 50, y);
+      g.lineTo(x + 100, y);
+      g.lineTo(x + 25, y - 150);
+      g.closePath();
+      g.fill();
+      g.stroke();
+  
+      let ballX = x;
+      let ballY = y;
+      for (let i = 0; i < 8; i++) {
+        this.drawBall(g, ballX, ballY);
+        ballX += 10;
+        ballY += 20;
+        if (ballX >= x + 20) {
+          ballX = x;
+        }
+      }
     }
-
-    optellen(x,y)
-    {
-        console.log(x + y);
-
+  
+    drawBall(g, x, y) {
+      this.setColor(g, "plum", 0.5);
+      g.beginPath();
+      g.arc(x + 20, y - 150, 10, 0, Math.PI * 2);
+      g.closePath();
+      g.fill();
+      g.stroke();
     }
-
-    extra()
-    {
-        console.log("hallo ik ben een extra functie");
-
+  
+    setColor(g, color, alpha = 1) {
+      g.fillStyle = color;
+      g.strokeStyle = color;
+      g.globalAlpha = alpha;
     }
-}
-
-
-let app = new App();
-app.runApplication();
+  }
+  
+  const app = new App();
+  app.runApp();
